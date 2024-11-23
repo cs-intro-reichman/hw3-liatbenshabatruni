2c26af4aadff33122690c4aea2c23f22a3172bc0
liatbenshabatruni <liat.benshabat@post.runi.ac.il>
2024-11-23 22:00:48 +0200

    q 1 +2

--- Algebra.java
+++ Algebra.java
@@ -6,39 +6,64 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
+	    /*System.out.println(plus(2,3));   // 2 + 3
+	    System.out.println(minus(7,2));  // 7 - 2 
+   		System.out.println(minus(2,7));  // 2 - 7 
  		System.out.println(times(3,4));  // 3 * 4
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
+   		System.out.println(pow(3,5));      // 3^5 
+   		System.out.println(div(12,3));   // 12 / 3   
    		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
+   		System.out.println(div(25,7));   // 25 / 7*/
+   		System.out.println(mod(-25,7));   // 25 % 7
+		System.out.println(mod(-25,-7));   // 25 % 7
+		System.out.println(mod(25,-7));   // 25 % 7
    		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36)); //6
-		System.out.println(sqrt(263169)); // 513
-   		System.out.println(sqrt(76123)); // 275
+   		//System.out.println(sqrt(36)); //6
+		//System.out.println(sqrt(263169)); // 513
+   		//System.out.println(sqrt(76123)); // 275
+		System.out.println("the correct"); 
+		System.out.println((-1*25)%7);
+		System.out.println((-1*25)%(-1*7));
+		System.out.println(25%-7);
+		System.out.println(120%6);
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		// creating a loop to run x2 times and each time it will add 1 to x1
-		for (int j = 0; j < x2; j++) {
+
+		if (x2<0)
+		{for (int j = x2 ; j < 0; j++) {
+			x1--;
+			
+			}
+		}
+		else {
+			for (int j = 0; j < x2; j++) {
 			x1++;
 			
+			}
 		}
-		return x1;
-	}
+			return x1;
+		}
+		
+	
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		// creating a loop to run x2 times and each time it will minus 1 from x1
-		for (int j = 0; j < x2; j++) {
+		if (x2<0) {
+			for (int j = x2; j < 0; j++) {
+			x1++;
+			}
+		}
+		else {
+			for (int j = 0; j < x2; j++) {
 			x1--;
 			
+			}
 		}
 		return x1;
 	}
@@ -46,12 +71,32 @@ public class Algebra {
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		// creating a loop to run x2-1 times and each time it will add num (origin x1) to x1 
-		int num = x1;
-		for (int j = 1; j < x2; j++) {
-			x1 = plus(x1, num);
-			
+		int num = 0;
+		if ((x1>0 && x2>0)){
+			num = x1;
+			for (int j = 1; j < x2; j++) {
+				num = plus(x1, num);
+			}
 		}
-		return x1;
+		if ((x2<0 && x1>0)){
+			num = x2;
+			for (int j = 1; j < x1; j++) {
+				num = plus(x2, num);
+			}
+		}
+		if (x1<0 && x2>0){
+			num = x1;
+			for (int j = 1; j < x2; j++) {
+				num = plus(x1, num);
+			}
+		}
+		if (x1<0 && x2<0){
+			num = x1;
+			for (int j = x2; j < 1; j++) {
+				num = minus(num,x1);
+			}
+		}
+		return num;
 	}
 
 	// Returns x^n (for n >= 0)
@@ -69,32 +114,57 @@ public class Algebra {
 	public static int div(int x1, int x2) {
 		// the loop will end when x1 is lower than x2 
 		int i = 0;
-		while (x2<=x1){
-			x1 = minus(x1, x2);
-			i++;
-
+		int num = 0;
+		int num1 = x1;
+		int num2 = x2;
+		if (x2<0){
+			num2 = times(-1, x2);
 		}
-		return i;
-	}
+		if (x1<0){
+			num1 = times(-1, x1);
+		}
+		num = num1;
+		while (num>=num2){
+			num = minus(num, num2);
+				i++;
+		}
+		if (((x1<0)&&(x2>0))||(x1>0)&&(x2<0)){
+			i = times(i, -1);
+		}
+			return i;
+		}
+	
 
 	// Returns x1 % x2
-	public static int mod(int x1, int x2) {
+	public static int mod(int x1, int x2){
 		// decrease x2 from x1 until x1 is lower and thare is the rest
-		while (x2<=x1){
-			x1 = minus(x1, x2);
-
+		int num = 0;
+		int num1 = x1;
+		int num2 = x2;
+		if (x2<0){
+			num2 = times(-1, x2);
 		}
-		return x1;
+		if (x1<0){
+			num1 = times(-1, x1);
+		}
+		num = num1;
+		while (num>=num2){
+			num = minus(num, num2);
+		}
+		
+		return num;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
 		// runing from 1 to x (num) until finding the num*num that would be X 
-		int num =1;
+		int num = 0;
 		while ((times(num, num)!=x)&&times(num, num)<=x){
 			num++;
 		}
 		
 			return num;
-	}	  	  
-}
+		}	
+	
+	}  	  
+	
