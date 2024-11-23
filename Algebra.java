ddc521edd75afcd4a2143778442f12a9a5ac3856
liatbenshabatruni <liat.benshabat@post.runi.ac.il>
2024-11-23 22:18:58 +0200

    One more time

--- Algebra.java
+++ Algebra.java
@@ -6,7 +6,7 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    /*System.out.println(plus(2,3));   // 2 + 3
+	    System.out.println(plus(2,3));   // 2 + 3
 	    System.out.println(minus(7,2));  // 7 - 2 
    		System.out.println(minus(2,7));  // 2 - 7 
  		System.out.println(times(3,4));  // 3 * 4
@@ -15,19 +15,12 @@ public class Algebra {
    		System.out.println(pow(3,5));      // 3^5 
    		System.out.println(div(12,3));   // 12 / 3   
    		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7*/
-   		System.out.println(mod(-25,7));   // 25 % 7
-		System.out.println(mod(-25,-7));   // 25 % 7
-		System.out.println(mod(25,-7));   // 25 % 7
+   		System.out.println(div(25,7));   // 25 / 7
+   		System.out.println(mod(25,7));   // 25 % 7
    		System.out.println(mod(120,6));  // 120 % 6    
-   		//System.out.println(sqrt(36)); //6
-		//System.out.println(sqrt(263169)); // 513
-   		//System.out.println(sqrt(76123)); // 275
-		System.out.println("the correct"); 
-		System.out.println((-1*25)%7);
-		System.out.println((-1*25)%(-1*7));
-		System.out.println(25%-7);
-		System.out.println(120%6);
+   		System.out.println(sqrt(36)); //6
+		System.out.println(sqrt(263169)); // 513
+   		System.out.println(sqrt(76123)); // 275
 	}  
 
 	// Returns x1 + x2
