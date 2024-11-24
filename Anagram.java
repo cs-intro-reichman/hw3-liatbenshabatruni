4ff5cc2cb7387393848a4ef3b05beb0522da2ef9
liatbenshabatruni <liat.benshabat@post.runi.ac.il>
2024-11-24 23:40:40 +0200

    the last one 3

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,56 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
-	}
+		str1 = preProcess(str1);
+		str2 = preProcess(str2);
+		//System.err.println(str1);
+		//System.err.println(str2);
+		int counter1 = 0;
+		int counter2 = 0;
+		for (int i = 0; i < str1.length(); i++) {
+			if ((str2.indexOf(str1.charAt(i))!= -1)) {
+					//strNew = strNew + str1.charAt(i);
+						counter1++;
+			}			
+		}
+		for (int i = 0; i < str2.length(); i++) {
+				if ((str1.indexOf(str2.charAt(i))!= -1)) {
+						counter2++;
+				}
+		}
+		//System.out.println(counter1);
+		//System.out.println(counter2);
+		return (counter1 == str1.length())&&(counter2 == str2.length());
+}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		// first, we'll delete the spaces, ?,!,:
+		String strNew = "";
+		str = str.toLowerCase();
+		for (int i = 0; i < str.length(); i++) {
+			if (((str.charAt(i)>=48) && (str.charAt(i)<=57))||((str.charAt(i)>=97)&&(str.charAt(i)<=122)))
+			{
+				strNew = strNew + str.charAt(i);
+			}
+			
+		}
+		return strNew;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
-	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+	public static String randomAnagram(String str) {	
+		int limit = str.length();
+		String strNew = "";
+		for (int i = 0; i < limit ; i++) {
+			int randomChar = (int)(Math.random()*str.length());
+			strNew = strNew + str.charAt(randomChar);
+			str = str.substring(0, randomChar) + str.substring(randomChar+1);
+			
+		}
+		return strNew;
 	}
 }
