a88a9e0f51434d936a2b68c5aaf4387d307d8a17
liatbenshabatruni <liat.benshabat@post.runi.ac.il>
2024-11-24 23:51:48 +0200

    last time Q3 amen

--- Anagram.java
+++ Anagram.java
@@ -58,7 +58,7 @@ public class Anagram {
 		String strNew = "";
 		str = str.toLowerCase();
 		for (int i = 0; i < str.length(); i++) {
-			if (((str.charAt(i)>=48) && (str.charAt(i)<=57))||((str.charAt(i)>=97)&&(str.charAt(i)<=122)))
+			if (((str.charAt(i)>=48) && (str.charAt(i)<=57))||((str.charAt(i)>=97)&&(str.charAt(i)<=122))||(str.charAt(i)==32))
 			{
 				strNew = strNew + str.charAt(i);
 			}
